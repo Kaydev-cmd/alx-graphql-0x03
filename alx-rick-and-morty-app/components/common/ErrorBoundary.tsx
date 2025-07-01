d6cb41457f11bf665cc65936eb3ca79e63f535a0
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-07-01 07:59:44 +0200

    feat: implement ErrorBoundary class with error handling lifecycle methods

--- alx-rick-and-morty-app/components/common/ErrorBoundary.tsx
+++ alx-rick-and-morty-app/components/common/ErrorBoundary.tsx
@@ -1,9 +1,25 @@
-import { ReactNode } from "react"
+import React from "react";
+import { ReactNode } from "react";
 
 interface State {
-  hasError: boolean
+  hasError: boolean;
 }
 
 interface ErrorBoundaryProps {
-  children: ReactNode
+  children: ReactNode;
+}
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, State> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error): State {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    console.log({ error, errorInfo });
+  }
 }
